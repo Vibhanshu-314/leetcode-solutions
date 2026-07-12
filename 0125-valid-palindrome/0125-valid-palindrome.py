3bd6fbe224756a804569ed8f45497c58640d8e11
VIBHANSHU KUMAR <161818554+Vibhanshu-314@users.noreply.github.com>
2026-07-12 22:22:37 +0530

    Time: 14 ms (76.08%), Space: 14.2 MB (15.86%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,5 +5,16 @@ class Solution(object):
         :rtype: bool
         """
         s=''.join(ch.lower() for ch in s  if ch.isalnum())
+# this comsumes extra space 
+        #return s==s[::-1]
+        left,right=0,len(s)-1
 
-        return s==s[::-1]
+        while left<right:
+            if s[left]!=s[right]:
+                return False
+            left+=1
+            right-=1
+
+
+
+        return True        
