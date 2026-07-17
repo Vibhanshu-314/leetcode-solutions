3b6d758a4adf7361278649e7a159d95fbbb47047
VIBHANSHU KUMAR <161818554+Vibhanshu-314@users.noreply.github.com>
2026-07-17 21:48:19 +0530

    Time: 0 ms (100%), Space: 12.4 MB (46.29%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -9,11 +9,26 @@ class Solution(object):
       #  words=s.strip().split()
       #  return len(words[-1])
 
-        s=s.strip()
-        count=0
+      # s=s.strip()
+      # count=0
+      # i=len(s)-1
+      # while i>=0 and s[i]!=" ":
+      #         count+=1
+      #         i-=1
+      # return count   
+        
         i=len(s)-1
+        
+            
+        while i>=0 and s[i]==" ":
+            i-=1
+        count=0
         while i>=0 and s[i]!=" ":
-                count+=1
-                i-=1
-        return count   
+            count+=1
+            i-=1
+        return count        
 
+
+    
+
+          
