9305c7b6763ecc5351b1625455cd3585cce1e820
Vibhanshu <161818554+Vibhanshu-314@users.noreply.github.com>
2026-08-14 10:33:27 +0530

    Time: 20 ms (49.68%), Space: 12.3 MB (53.92%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -3,7 +3,20 @@ class Solution(object):
         """
         :type n: int
         :rtype: int
-        """
-        if n<=1:
-            return n
-        return self.fib(n-1) + self.fib(n-2)       
+        """ 
+        # recursion
+
+       #if n<=1:
+       #    return n
+       #return self.fib(n-1) + self.fib(n-2)       
+
+        # memoziation 
+        memo={}
+        def dfs(n):
+            if n<=1:
+                return n
+            if n in memo:
+                return memo[n] 
+            memo[n]=dfs(n-1)+dfs(n-2)
+            return memo[n]
+        return dfs(n)       
